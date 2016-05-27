06abdec24752f94732c85bc3523291a31ea1842f
venugopal <myphpwordpress@gmail.com>
2016-05-27 16:05:32 +0530

    Files uploaded

---
+++ custom.js
@@ -0,0 +1,16 @@
+    jQuery(document).ready(function () {
+   /**Inner page Chat section Start */
+ 
+
+    jQuery(".show-right").click(function () {
+        jQuery('.chat-box').toggleClass("chat-right-zero");
+    });
+    /**Inner page Chat section END*/
+    
+    jQuery(".close_right").click(function () {
+         jQuery('.chat-box').toggleClass("chat-right-zero");
+    });
+    
+    
+    
+});
